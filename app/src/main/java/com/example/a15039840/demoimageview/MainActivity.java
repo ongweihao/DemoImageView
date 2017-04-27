f8048569d2f265056dd9042ccab8db4d8a75cdd0
15039840 <15039840@myrp.edu.sg>
2017-04-27 10:23:40 +0800

    Commiting

--- app/src/main/java/com/example/a15039840/demoimageview/MainActivity.java
+++ app/src/main/java/com/example/a15039840/demoimageview/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.a15039840.demoimageview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView ivDay2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ivDay2 = (ImageView)findViewById(R.id.imageView2);
+
+        ivDay2.setImageResource(R.drawable.day2);
     }
 }
